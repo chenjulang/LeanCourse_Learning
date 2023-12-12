601d60cf5b0f70ff5e416b9c9eff1bbd058fd5f6
陈聚烺 <565027193@qq.com>
2023-12-12 09:56:09 +0800

    1

--- LeanCourse/LinearAlgebra/LA1_8_wrong.lean
+++ LeanCourse/LinearAlgebra/LA1_8_wrong.lean
@@ -1,21 +1,18 @@
 import Mathlib.Algebra.Algebra.Opposite
-import Mathlib.Algebra.Algebra.Pi
-import Mathlib.Algebra.Module.LinearMap
-import Mathlib.Algebra.Module.Pi
+-- import Mathlib.Algebra.Algebra.Pi
+-- import Mathlib.Algebra.BigOperators.Pi
+-- import Mathlib.Algebra.BigOperators.Ring
+-- import Mathlib.Algebra.BigOperators.RingEquiv
+-- import Mathlib.Algebra.Module.LinearMap
+-- import Mathlib.Algebra.Module.Pi
 import Mathlib.Algebra.Star.BigOperators
 import Mathlib.Algebra.Star.Module
 import Mathlib.Algebra.Star.Pi
-import Mathlib.Algebra.BigOperators.Pi
-import Mathlib.Algebra.BigOperators.Ring
-import Mathlib.Algebra.BigOperators.RingEquiv
-import Mathlib.Data.Fintype.BigOperators
+-- import Mathlib.Data.Fintype.BigOperators
 -- 单文件示例
 -- 参考lake-packages/mathlib/Mathlib/Data/Matrix/Basic.lean
 
 universe u u' v w
-
-open BigOperators
-
 
 def Matrix (m : Type u) (n : Type u') (α : Type v): Type max u u' v
 :=
@@ -618,7 +615,8 @@ theorem diagonal_smul [Monoid R] [AddMonoid α] [DistribMulAction R α] (r : R) 
 variable (n α)
 
 
-@[simps] -- 注意这个符号 →+， 和“加幺半群”有关
+/-- `Matrix.diagonal` as an `AddMonoidHom`. -/
+@[simps]
 def diagonalAddMonoidHom [AddZeroClass α] : (n → α) →+ Matrix n n α where
   toFun := diagonal
   map_zero' := diagonal_zero
@@ -627,370 +625,43 @@ def diagonalAddMonoidHom [AddZeroClass α] : (n → α) →+ Matrix n n α where
 
 variable (R)
 
-@[simps] -- 线性映射
+/-- `Matrix.diagonal` as a `LinearMap`. -/
+@[simps]
 def diagonalLinearMap [Semiring R] [AddCommMonoid α] [Module R α] : (n → α) →ₗ[R] Matrix n n α :=
   { diagonalAddMonoidHom n α with map_smul' := diagonal_smul }
+#align matrix.diagonal_linear_map Matrix.diagonalLinearMap
 
 variable {n α R}
 
 @[simp]
-theorem diagonal_map [Zero α] [Zero β] {f : α → β} (h : f 0 = 0) {d : n → α} : (diagonal d).map f = diagonal fun m => f (d m) := by
+theorem diagonal_map [Zero α] [Zero β] {f : α → β} (h : f 0 = 0) {d : n → α} :
+    (diagonal d).map f = diagonal fun m => f (d m) := by
   ext
   simp only [diagonal_apply, map_apply]
   split_ifs <;> simp [h]
+#align matrix.diagonal_map Matrix.diagonal_map
 
 
-@[simp] -- star 类似复共轭这样的运算
-theorem diagonal_conjTranspose [AddMonoid α] [StarAddMonoid α] (v : n → α) : (diagonal v)ᴴ = diagonal (star v)
-:= by
+@[simp]
+theorem diagonal_conjTranspose [AddMonoid α] [StarAddMonoid α] (v : n → α) :
+    (diagonal v)ᴴ = diagonal (star v) := by
   rw [conjTranspose, diagonal_transpose, diagonal_map (star_zero _)]
   rfl
+#align matrix.diagonal_conj_transpose Matrix.diagonal_conjTranspose
 
-section One
 
-variable [Zero α] [One α]
 
-instance one : One (Matrix n n α) :=
-  ⟨diagonal fun _ => 1⟩
 
-@[simp]
-theorem diagonal_one : (diagonal fun _ => 1 : Matrix n n α) = 1 :=
-  rfl
 
-theorem one_apply {i j} : (1 : Matrix n n α) i j = if i = j then 1 else 0 :=
-  rfl
 
-@[simp]
-theorem one_apply_eq (i) : (1 : Matrix n n α) i i = 1 :=
-  diagonal_apply_eq _ i
 
-@[simp]
-theorem one_apply_ne {i j} : i ≠ j → (1 : Matrix n n α) i j = 0 :=
-  diagonal_apply_ne _
-
-theorem one_apply_ne' {i j} : j ≠ i → (1 : Matrix n n α) i j = 0 :=
-  diagonal_apply_ne' _
-
-@[simp]
-theorem map_one [Zero β] [One β] (f : α → β) (h₀ : f 0 = 0) (h₁ : f 1 = 1) : (1 : Matrix n n α).map f = (1 : Matrix n n β) := by
-  ext
-  simp only [one_apply, map_apply]
-  split_ifs <;> simp [h₀, h₁]
-
-theorem one_eq_pi_single {i j} : (1 : Matrix n n α) i j = Pi.single (f := fun _ => α) i 1 j
-:= by
-  simp only [one_apply, Pi.single_apply, eq_comm]
-
-end One
-
-section Numeral
-
--- 弃用警告 关闭
-set_option linter.deprecated false
-
-@[deprecated, simp]
-theorem bit0_apply [Add α] (M : Matrix m m α) (i : m) (j : m) : (bit0 M) i j = bit0 (M i j)
-:=
-  rfl
-
-variable [AddZeroClass α] [One α]
-
-@[deprecated]
-theorem bit1_apply (M : Matrix n n α) (i : n) (j : n) :
-    (bit1 M) i j = if i = j then bit1 (M i j) else bit0 (M i j) := by
-  dsimp [bit1]
-  by_cases h : i = j <;>
-  simp [h]
-
-@[deprecated, simp]
-theorem bit1_apply_eq (M : Matrix n n α) (i : n) : (bit1 M) i i = bit1 (M i i)
-:= by
-  simp [bit1_apply]
-
-@[deprecated, simp]
-theorem bit1_apply_ne (M : Matrix n n α) {i j : n} (h : i ≠ j) : (bit1 M) i j = bit0 (M i j)
-:= by
-  simp [bit1_apply, h]
-#align matrix.bit1_apply_ne Matrix.bit1_apply_ne
-
-end Numeral
 
 end Diagonal
 
 
-section Diag
 
--- 正方阵的对角线
--- @[simp] -- Porting note: simpNF does not like this.
-def diag (A : Matrix n n α) (i : n) : α :=
-  A i i
 
 
-@[simp]
-theorem diag_apply (A : Matrix n n α) (i) : diag A i = A i i
-:=
-  rfl
-
-@[simp]
-theorem diag_diagonal [DecidableEq n] [Zero α] (a : n → α) : diag (diagonal a) = a
-:=
-  funext <| @diagonal_apply_eq _ _ _ _ a
-
-@[simp]
-theorem diag_transpose (A : Matrix n n α) : diag Aᵀ = diag A :=
-  rfl
-
-@[simp]
-theorem diag_zero [Zero α] : diag (0 : Matrix n n α) = 0 :=
-  rfl
-
-@[simp]
-theorem diag_add [Add α] (A B : Matrix n n α) : diag (A + B) = diag A + diag B :=
-  rfl
-
-@[simp]
-theorem diag_sub [Sub α] (A B : Matrix n n α) : diag (A - B) = diag A - diag B :=
-  rfl
-
-@[simp]
-theorem diag_neg [Neg α] (A : Matrix n n α) : diag (-A) = -diag A :=
-  rfl
-
-@[simp]
-theorem diag_smul [SMul R α] (r : R) (A : Matrix n n α) : diag (r • A) = r • diag A :=
-  rfl
-
-@[simp]
-theorem diag_one [DecidableEq n] [Zero α] [One α] : diag (1 : Matrix n n α) = 1 :=
-  diag_diagonal _
-
-variable (n α)
-
-/-- 加幺半群`Matrix.diag` as an `AddMonoidHom`. -/
-@[simps]
-def diagAddMonoidHom [AddZeroClass α] : Matrix n n α →+ n → α where
-  toFun := diag
-  map_zero' := diag_zero
-  map_add' := diag_add
-
-variable (R)
-
--- 线性映射
-@[simps]
-def diagLinearMap [Semiring R] [AddCommMonoid α] [Module R α] : Matrix n n α →ₗ[R] n → α
-:=
-  { diagAddMonoidHom n α with map_smul' := diag_smul }
-
-variable {n α R}
-
-theorem diag_map {f : α → β} {A : Matrix n n α} : diag (A.map f) = f ∘ diag A
-:=
-  rfl
-
-@[simp]
-theorem diag_conjTranspose [AddMonoid α] [StarAddMonoid α] (A : Matrix n n α) : diag Aᴴ = star (diag A)
-:=
-  rfl
-
-@[simp]
-theorem diag_list_sum [AddMonoid α] (l : List (Matrix n n α)) : diag l.sum = (l.map diag).sum
-:=
-  map_list_sum (diagAddMonoidHom n α) l
-
--- 即“求和 s 中所有矩阵的对角线元素之和”的结果等于“把 s 中的每个矩阵映射为其对角矩阵再求和”的结果。
-@[simp]
-theorem diag_multiset_sum [AddCommMonoid α] (s : Multiset (Matrix n n α)) : diag s.sum = (s.map diag).sum :=
-  map_multiset_sum (diagAddMonoidHom n α) s
-
--- 对于集合s中的每个元素f i，对其取对角线元素并求和得到∑ i in s, diag (f i)；
--- 对集合s中的所有元素f i 求和，并取其对角线元素得到diag (∑ i in s, f i)；
--- 两个结果相等。
-@[simp]
-theorem diag_sum {ι} [AddCommMonoid α] (s : Finset ι) (f : ι → Matrix n n α) : diag (∑ i in s, f i) = ∑ i in s, diag (f i)
-:=
-  map_sum (diagAddMonoidHom n α) f s
-
-end Diag --/
-
-section DotProduct --\
-
-variable [Fintype m] [Fintype n]
-
--- 逐项乘积，也就是点积
-def dotProduct [Mul α] [AddCommMonoid α] (v w : m → α) : α :=
-  ∑ i, v i * w i
-
-@[inherit_doc]
-scoped infixl:72 " ⬝ᵥ " => Matrix.dotProduct
-
--- 点积的结合律
-theorem dotProduct_assoc [NonUnitalSemiring α] (u : m → α) (w : n → α) (v : Matrix m n α) : ( (fun j => (u) ⬝ᵥ (fun i => v i j)) ⬝ᵥ w )  =  ( u ⬝ᵥ fun i => v i ⬝ᵥ w )
-:= by
-  simpa [dotProduct, Finset.mul_sum, Finset.sum_mul, mul_assoc] using Finset.sum_comm
-
---点积的交换律
-theorem dotProduct_comm [AddCommMonoid α] [CommSemigroup α] (v w : m → α) : v ⬝ᵥ w = w ⬝ᵥ v
-:= by
-simp_rw [dotProduct, mul_comm]
-
--- PUnit 是一个类型，它只有一个元素，用 ⟨⟩ 表示出
--- 假设有两个函数 v 和 w，它们都从类型为 PUnit 到类型为 α 的值域。由于 PUnit 只有一个元素 ⟨⟩，所以这两个函数在 ⟨⟩ 上的取值分别为 v ⟨⟩ 和 w ⟨⟩。
--- 这个定理的表述是说，点积运算 v ⬝ᵥ w 的结果等于将这两个函数在 ⟨⟩ 上的取值相乘。也就是说，v ⬝ᵥ w 等于 v ⟨⟩ * w ⟨⟩。
--- 点积是一种向量运算，用于计算两个向量之间的相似性或者进行向量的变换。在这个定理中，点积的定义是将两个函数在所有可能输入的位置上的值相乘并求和。然而，由于 PUnit 只有一个元素，所以 v 和 w 的取值只与 ⟨⟩ 相关。
--- 因此，这个定理实际上在说，当向量的输入空间只有一个元素时，点积运算可以简化为将两个函数在该元素上的取值相乘。
-@[simp]
-theorem dotProduct_pUnit [AddCommMonoid α] [Mul α] (v w : PUnit → α) : v ⬝ᵥ w = v ⟨⟩ * w ⟨⟩
-:= by
-  simp [dotProduct]
-
-section MulOneClass
-
-variable [MulOneClass α] [AddCommMonoid α]
-
-theorem dotProduct_one (v : n → α) : v ⬝ᵥ 1 = ∑ i, v i
-:= by simp [(· ⬝ᵥ ·)]
-
-theorem one_dotProduct (v : n → α) : 1 ⬝ᵥ v = ∑ i, v i
-:= by simp [(· ⬝ᵥ ·)]
-
-end MulOneClass
-
-section NonUnitalNonAssocSemiring
-
-variable [NonUnitalNonAssocSemiring α] (u v w : m → α) (x y : n → α)
-
-@[simp]
-theorem dotProduct_zero : v ⬝ᵥ 0 = 0
-:= by simp [dotProduct]
-
-@[simp]
-theorem dotProduct_zero' : (v ⬝ᵥ fun _ => 0) = 0
-:=
-  dotProduct_zero v
-
-@[simp]
-theorem zero_dotProduct : 0 ⬝ᵥ v = 0
-:= by simp [dotProduct]
-
-@[simp]
-theorem zero_dotProduct' : (fun _ => (0 : α)) ⬝ᵥ v = 0
-:=
-  zero_dotProduct v
-
--- 点积的分配律
-@[simp]
-theorem add_dotProduct : (u + v) ⬝ᵥ w = u ⬝ᵥ w + v ⬝ᵥ w
-:= by
-  simp [dotProduct, add_mul, Finset.sum_add_distrib]
-
-@[simp]
-theorem dotProduct_add : u ⬝ᵥ (v + w) = u ⬝ᵥ v + u ⬝ᵥ w := by
-  simp [dotProduct, mul_add, Finset.sum_add_distrib]
-
--- 假设有 u = [1, 2]、v = [3, 4]、x = [5, 6] 和 y = [7, 8]，这些数字代表了四个向量。现在我们想要计算 Sum.elim u x ⬝ᵥ Sum.elim v y 的值。
--- 首先， Sum.elim u x 表示将两个向量 u = [1, 2] 和 x = [5, 6] 进行组合，得到新的向量 [1, 2, 5, 6]；Sum.elim v y 表示将两个向量 v = [3, 4] 和 y = [7, 8] 进行组合，得到新的向量 [3, 4, 7, 8]。
--- 接下来，我们将新得到的两个向量 [1, 2, 5, 6] 和 [3, 4, 7, 8] 进行点积运算。根据点积的定义，这个点积的计算方式如下：
--- (1 * 3) + (2 * 4) + (5 * 7) + (6 * 8) = 3 + 8 + 35 + 48 = 94
--- 同样，我们也可以直接计算 u ⬝ᵥ v + x ⬝ᵥ y 的值：
--- (1 * 3 + 2 * 4) + (5 * 7 + 6 * 8) = 3 + 8 + 35 + 48 = 94
-@[simp]
-theorem sum_elim_dotProduct_sum_elim : (Sum.elim u x) ⬝ᵥ (Sum.elim v y) = u ⬝ᵥ v + x ⬝ᵥ y
-:= by
-  simp [dotProduct]
-
-
--- 举一个实际的例子。
--- 假设在一个二维坐标系中，有两个点 (1, 2) 和 (3, 4)（即一维坐标为 1 和 3，二维坐标为 2 和 4）。我们可以将这两个点分别表示为 m 中的向量 u = [1, 2] 和 x = [3, 4]。现在我们想要将这两个点从一个坐标系之间映射到另一个坐标系。
--- 首先，我们有一个双射 e，这个双射将第一个坐标系上的点 (1, 2) 映射到第二个坐标系的点 (4, 5) 上，将第一个坐标系上的点 (3, 4) 映射到第二个坐标系中的点 (6, 7) 上。那么，我们可以使用双射 e 将向量 u 和 x 转换为类型为 n 的向量，表示它们在第二个坐标系中的位置关系。即：
--- u 通过 e 映射为向量 u' = [4, 5]
--- x 通过 e 映射为向量 x' = [6, 7]
--- 接下来，我们可以用这两个向量在第二个坐标系中进行点积运算，得到它们在空间中的位置关系。即：
--- u' ⬝ᵥ x' = (4 * 6) + (5 * 7) = 62
--- 如果我们按照原始的方式，将点在第一个坐标系中的位置通过点积运算得到它们在空间中的位置关系，也可以应用双射 e 将点转换为类型为 n 的向量，再用它们在第二个坐标系中的向量进行点积运算。即：
--- u 在第一个坐标系中的位置为 (1,2)，那么向量 u 在 e 映射下变成 u' = [4, 5]
--- x 在第一个坐标系中的位置为 (3, 4)，那么向量 x 在 e 映射下变成 x' = [6, 7]
--- 然后，使用这两个类型为 n 的向量进行点积运算，即 u' ⬝ᵥ x' = 62。
--- 可以看到，无论是将两个点直接进行点积运算，还是先将它们进行双射 e 的映射，得到对应的向量，再进行点积运算，最终得到的结果都是相同的，即点积的结果是 62，表示这两个点之间的相似度为 62。这说明了该定理在空间变换中的应用。
-@[simp]
-theorem comp_equiv_symm_dotProduct (e : m ≃ n) : u ∘ e.symm ⬝ᵥ x = u ⬝ᵥ x ∘ e
-:=
-  (e.sum_comp _).symm.trans <|
-    Finset.sum_congr rfl fun _ _ => by simp only [Function.comp, Equiv.symm_apply_apply]
-
-/-- Permuting a vector on the right of a dot product can be transferred to the left. -/
-@[simp]
-theorem dotProduct_comp_equiv_symm (e : n ≃ m) : u ⬝ᵥ x ∘ e.symm = u ∘ e ⬝ᵥ x := by
-  simpa only [Equiv.symm_symm] using (comp_equiv_symm_dotProduct u x e.symm).symm
-#align matrix.dot_product_comp_equiv_symm Matrix.dotProduct_comp_equiv_symm
-
-/-- Permuting vectors on both sides of a dot product is a no-op. -/
-@[simp]
-theorem comp_equiv_dotProduct_comp_equiv (e : m ≃ n) : x ∘ e ⬝ᵥ y ∘ e = x ⬝ᵥ y := by
-  -- Porting note: was `simp only` with all three lemmas
-  rw [← dotProduct_comp_equiv_symm]; simp only [Function.comp, Equiv.apply_symm_apply]
-#align matrix.comp_equiv_dot_product_comp_equiv Matrix.comp_equiv_dotProduct_comp_equiv
-
-end NonUnitalNonAssocSemiring
-
-section NonUnitalNonAssocSemiringDecidable
-
-variable [DecidableEq m] [NonUnitalNonAssocSemiring α] (u v w : m → α)
-
-@[simp]
-theorem diagonal_dotProduct (i : m) : diagonal v i ⬝ᵥ w = v i * w i := by
-  have : ∀ (j) (_ : j ≠ i), diagonal v i j * w j = 0 := fun j hij => by
-    simp [diagonal_apply_ne' _ hij]
-  convert Finset.sum_eq_single i (fun j _ => this j) _ using 1 <;> simp
-#align matrix.diagonal_dot_product Matrix.diagonal_dotProduct
-
-@[simp]
-theorem dotProduct_diagonal (i : m) : v ⬝ᵥ diagonal w i = v i * w i := by
-  have : ∀ (j) (_ : j ≠ i), v j * diagonal w i j = 0 := fun j hij => by
-    simp [diagonal_apply_ne' _ hij]
-  convert Finset.sum_eq_single i (fun j _ => this j) _ using 1 <;> simp
-#align matrix.dot_product_diagonal Matrix.dotProduct_diagonal
-
-@[simp]
-theorem dotProduct_diagonal' (i : m) : (v ⬝ᵥ fun j => diagonal w j i) = v i * w i := by
-  have : ∀ (j) (_ : j ≠ i), v j * diagonal w j i = 0 := fun j hij => by
-    simp [diagonal_apply_ne _ hij]
-  convert Finset.sum_eq_single i (fun j _ => this j) _ using 1 <;> simp
-#align matrix.dot_product_diagonal' Matrix.dotProduct_diagonal'
-
-@[simp]
-theorem single_dotProduct (x : α) (i : m) : Pi.single i x ⬝ᵥ v = x * v i := by
-  -- Porting note: (implicit arg) added `(f := fun _ => α)`
-  have : ∀ (j) (_ : j ≠ i), Pi.single (f := fun _ => α) i x j * v j = 0 := fun j hij => by
-    simp [Pi.single_eq_of_ne hij]
-  convert Finset.sum_eq_single i (fun j _ => this j) _ using 1 <;> simp
-#align matrix.single_dot_product Matrix.single_dotProduct
-
-@[simp]
-theorem dotProduct_single (x : α) (i : m) : v ⬝ᵥ Pi.single i x = v i * x := by
-  -- Porting note: (implicit arg) added `(f := fun _ => α)`
-  have : ∀ (j) (_ : j ≠ i), v j * Pi.single (f := fun _ => α) i x j = 0 := fun j hij => by
-    simp [Pi.single_eq_of_ne hij]
-  convert Finset.sum_eq_single i (fun j _ => this j) _ using 1 <;> simp
-#align matrix.dot_product_single Matrix.dotProduct_single
-
-end NonUnitalNonAssocSemiringDecidable
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-end DotProduct
 
 
 end Matrix --/
