bc19e41dec073abc34ee8492165c3853198d0270
陈聚烺 <565027193@qq.com>
2023-12-25 15:11:47 +0800

    1

--- LeanCourse/Video/Learning4.lean
+++ LeanCourse/Video/Learning4.lean
@@ -33,7 +33,35 @@ noncomputable section
       -- Submodule R M：环 R 上的模 M 的子模组成的集合类型
     def LinearIndependent2 : Prop :=
       LinearMap.ker (Finsupp.total ι M R v) = ⊥ -- 在线性代数中，⊥ 经常用于表示零空间或零子空间。
-    -- todo翻了3层定义才搞懂了
+    -- 举例说明
+    -- 已知2个条件：
+    -- 已知任意的一维向量d，即(x,y),举例（7，8）
+    -- 已知一个映射v，将（1，0）=>(2,3),将（0，1）=> (4,5)
+    -- 推理：
+    -- Finsupp.total ι M R v具体结果就是：fun d => d.sum fun i => F i
+    -- d.sum (fun i => F i) 具体做的是：
+    -- 展开d.sum: ∑ a in d.support, g a (d a)
+    -- 展开g，g就是(fun i => F i)：∑ a in d.support, (fun i => F i) a (d a)
+    -- a是什么，先看d.support是指d中有非零元素的索引位置的集合，即{1,2},因为(7，8)的第1，2位都是非零数
+    -- 所以展开a就是
+    -- (fun i => F i) 1 (d 1) +
+    -- (fun i => F i) 2 (d 2)
+    -- 代入(fun i => F i）得到,并且代入变量d：(F 1) (7) + (F 2) (8)
+    -- F是什么呢？是LinearMap.id.smulRight (v i)
+    -- LinearMap.id.smulRight (v 1) (7) + LinearMap.id.smulRight (v 2) (8)
+    -- 这里不代入比较好，变回由已知条件变量d，映射v来表示：
+    -- LinearMap.id.smulRight (v 1) (d 1) + LinearMap.id.smulRight (v 2) (d 2)
+    -- 即(v 1)•(d 1) + (v 2)•(d 2)
+    -- 这就是(Finsupp.total ι M R v)映射具体代表的，它将任意的向量d，变成了向量(v 1)•(d 1) + (v 2)•(d 2)
+    -- 因此LinearMap.ker (Finsupp.total ι M R v)指的是由上述映射，从像为零向量，反向映射回去后，得到的所有向量，只有零向量一个。
+    -- 则ι M R v共同形成一个关系：线性无关
+    -- v : ι → M 指的是一个映射，根据这里举的例子，将（1，0）=>(2,3),将（0，1）=> (4,5)，
+      -- ι就是第一个向量空间（一维2项向量组成的集合），M指的是第二个向量空间（一维2项向量组成的集合）
+    -- R应该指的是数乘取的元素的集合，比如我们举例就是R实数
+
+
+
+
 
     theorem linearIndependent2_iff : LinearIndependent2 R v ↔ ∀ l, Finsupp.total ι M R v l = 0 → l = 0 :=
       by simp [LinearIndependent2, LinearMap.ker_eq_bot']
