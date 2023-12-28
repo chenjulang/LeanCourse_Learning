28f32cb0f6f71242f4606d1395159e48330b2997
陈聚烺 <565027193@qq.com>
2023-12-28 18:49:45 +0800

    1

--- LeanCourse/Video/Learning6.lean
+++ LeanCourse/Video/Learning6.lean
@@ -1,28 +1,52 @@
 import Mathlib.LinearAlgebra.Matrix.ToLin
-
-
-noncomputable section
+import Mathlib.LinearAlgebra.Span
 
 open LinearMap Matrix Set Submodule
-
 open BigOperators
-
 open Matrix
-
 universe u v w
 
-instance {n m} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n] (R) [Fintype R] :
-    Fintype (Matrix m n R) := by unfold Matrix; infer_instance
+-- instance {n m} [Fintype m] [DecidableEq m] [Fintype n] [DecidableEq n] (R) [Fintype R] :
+--     Fintype (Matrix m n R) := by unfold Matrix; infer_instance
+--span的参数
+variable {R R₂ K M M₂ V S : Type*}
+
+-- namespace Submodule
+open Function Set
+open Pointwise
+
+  -- section AddCommMonoid
+
+variable [Semiring R] [AddCommMonoid M] [Module R M]
+variable {x : M} (p p' : Submodule R M)
+variable [Semiring R₂] {σ₁₂ : R →+* R₂}
+variable [AddCommMonoid M₂] [Module R₂ M₂] {F : Type*} [SemilinearMapClass F σ₁₂ M M₂]
+
+  section
+    variable (R)
+
+    def span2 (s : Set M) : Submodule R M :=
+    sInf { p | s ⊆ p }
+
+  end
+
+  -- end AddCommMonoid
+
+-- end Submodule
+
+
+
+--
 
 -- 生成集，最终证明矩阵的各列生成Rn空间
 section ToMatrix'
 
-variable {R : Type*} [CommSemiring R]
+  variable {R : Type*} [CommSemiring R]
 
-variable {k l m n : Type*} [DecidableEq n] [Fintype n]
+  variable {k l m n : Type*} [DecidableEq n] [Fintype n]
 
-theorem Matrix.range_toLin' (M : Matrix m n R) :
-    LinearMap.range (Matrix.toLin' M) = span R (range Mᵀ) :=
-  Matrix.range_mulVecLin _
+  theorem Matrix.range_toLin2' (M : Matrix m n R) :
+      LinearMap.range (Matrix.toLin' M) = span2 R (range Mᵀ) :=
+    Matrix.range_mulVecLin _
 
-end
+end ToMatrix'
