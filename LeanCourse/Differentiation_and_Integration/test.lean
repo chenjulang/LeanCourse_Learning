19a084685d6123dc71c744a0d4f35e6e81b1335c
陈聚烺 <565027193@qq.com>
2024-04-08 16:15:38 +0800

    1

--- LeanCourse/Differentiation_and_Integration/test.lean
+++ LeanCourse/Differentiation_and_Integration/test.lean
@@ -4,16 +4,18 @@
 -- 3.从mathlib里面熟悉的定理开始反向学习，追究原理深度2-3层最好，
   -- 这样掌握起来会比较快，而且有实际应用，有成就感。
 
--- 出发点1：lake-packages/mathlib/Mathlib/Analysis/SpecialFunctions/Integrals.lean
+-- 出发点1：数学分析内容整理：lake-packages/mathlib/Mathlib/Analysis/SpecialFunctions/Integrals.lean
 -- sin计算值证明： /Users/chenjulang/Desktop/数学/LeanCourse_Learning/lake-packages/mathlib/Mathlib/Analysis/SpecialFunctions/Trigonometric/Angle.lean
 -- 积分计算例子：
 -- integral_cos_mul_cos_pow_aux
 --  integral_gaussian_complex_Ioi
--- 出发点2:Lecture13节课
--- 出发点3:MIL的Structure开始重看，
-  -- 或者Kevin的课程：https://www.ma.imperial.ac.uk/~buzzard/xena/formalising-mathematics-2024/Introduction/introduction.html
--- 出发点4：有没有低观点的定义出发的证明？
-
+-- 出发点2:***Lecture13节课，或者MIL的Structure开始重看。
+-- 出发点3:
+  -- 群里发过的小问题项目
+  -- ***或者Kevin的课程：https://www.ma.imperial.ac.uk/~buzzard/xena/formalising-mathematics-2024/Introduction/introduction.html
+  -- ***硕士生Lean4教程：https://github.com/adomani/MA4N1_2023
+-- 出发点4：有没有低观点的定义出发的证明？LADR4
+-- 出发点5:陶哲轩从零学习Lean4过程记录23年10月10日开始：链接：https://mathstodon.xyz/@tao
 
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
 
@@ -21,6 +23,6 @@ open Real
 
 noncomputable section s1
 
-#check sin 0
+#check sin 0 = 0
 
 end s1
